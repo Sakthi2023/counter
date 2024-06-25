f49b73a94d854f08ea2088761219f674ba455426
Sakthi2023 <sakthinaveen55@gmail.com>
2024-06-25 09:24:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>Count: 0</p>
+      <button>Increment</button>
+      <button>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
